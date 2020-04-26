1933ec45cc20a51f77188748b023a0e70fdd8126
uic34828 <marina.andjelkovic@continental-corporation.com>
2020-04-26 16:48:16 +0200

    mvvm

--- app/src/main/java/com/example/simplemvijava/ui/DataStateListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.simplemvijava.ui;
-
-import com.example.simplemvijava.util.DataState;
-
-public interface DataStateListener {
-
-    void onDataStateChange(DataState<?> dataState);
-}
--- app/src/main/java/com/example/simplemvijava/ui/MainActivity.java
+++ app/src/main/java/com/example/simplemvijava/ui/MainActivity.java
@@ -5,33 +5,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.simplemvijava.R;
-import com.example.simplemvijava.model.Blog;
-import com.example.simplemvijava.ui.state.MainStateEvent;
-import com.example.simplemvijava.ui.state.MainViewState;
-import com.example.simplemvijava.util.DataState;
-import com.example.simplemvijava.util.Event;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity implements DataStateListener, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private List<Blog> blogList = new ArrayList<>();
-
     private MainViewModel viewModel;
-    private DataStateListener dataStateHandler;
     private BlogRecyclerAdapter blogListRecyclerAdapter;
 
     @Override
@@ -41,20 +27,19 @@ public class MainActivity extends AppCompatActivity implements DataStateListener
 
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
-        setDataStateHandler();
-
         setUI();
 
-        subscribeObservers();
+        observeBlogs();
     }
 
-    private void setDataStateHandler() {
-        try {
-            dataStateHandler = this; // context
-        }
-        catch (ClassCastException e) {
-            Log.d(TAG, "setDataStateHandler: " + this + " must implement DataStateListener");
-        }
+    private void observeBlogs() {
+        viewModel.getBlogsListLiveData().observe(this, blogs -> {
+
+            blogListRecyclerAdapter.submitList(blogs);
+            Log.d(TAG, "blogs: " + blogs);
+
+            showProgressBar(false);
+        });
     }
 
     private void initRecyclerView() {
@@ -72,62 +57,6 @@ public class MainActivity extends AppCompatActivity implements DataStateListener
         initRecyclerView();
     }
 
-    // subscribe to all the observers in the view model
-    private void subscribeObservers() {
-
-        // new Observer<DataState<MainViewState>>()
-        viewModel.getDataStateLiveData().observe(this, dataState -> {
-            if (dataState != null) {
-                Log.d(TAG, "DEBUG: onChanged: DataState: " + dataState.toString());
-
-                // Handle Loading and Message
-                dataStateHandler.onDataStateChange(dataState);
-
-                // handle Data<T>
-                if (dataState.getData() != null) {
-
-                    MainViewState mainViewState = dataState.getData().getContentIfNotHandled();
-
-                    if (mainViewState != null) {
-                        Log.d(TAG, "DEBUG: MainViewState: " + mainViewState.toString());
-
-                        if (mainViewState.getBlogs() != null) {
-                            blogList = mainViewState.getBlogs();
-                            viewModel.setBlogListData(blogList);
-                        }
-                    }
-                }
-            }
-        });
-
-        // observe(this, new Observer<MainViewState>()
-        viewModel.getViewStateLiveData().observe(this, mainViewState -> {
-
-            if (mainViewState.getBlogs() != null) {
-                Log.d(TAG, "DEBUG: Setting blog posts to RecyclerView: " + mainViewState.getBlogs());
-                blogListRecyclerAdapter.submitList(mainViewState.getBlogs());
-            }
-        });
-    }
-
-    @Override
-    public void onDataStateChange(DataState<?> dataState) {
-        handleDataStateChange(dataState);
-    }
-
-    private void handleDataStateChange(@Nullable DataState<?> dataState) {
-        if (dataState != null) {
-            // Handle loading
-            showProgressBar(dataState.getLoading());
-            Log.d(TAG, "DEBUG: dataState.getLoading() " + dataState.getLoading());
-
-            // Handle Message
-            if (dataState.getMessage() != null) {
-                showToast(dataState.getMessage());
-            }
-        }
-    }
-
     private void showProgressBar(Boolean isVisible) {
         ProgressBar progressBar = findViewById(R.id.progress_bar);
         if (isVisible) {
@@ -138,17 +67,11 @@ public class MainActivity extends AppCompatActivity implements DataStateListener
         }
     }
 
-    private void showToast(Event<String> message) {
-        Toast.makeText(this, message.toString(), Toast.LENGTH_SHORT).show();
-    }
-
     @Override
     public void onClick(View v) {
-        Log.d(TAG, "clicked on button to load blogs");
-        triggerGetBlogsEvent();
-    }
+        showProgressBar(true);
 
-    private void triggerGetBlogsEvent() {
-        viewModel.setStateEvent(new MainStateEvent.GetBlogsEvent());
+        Log.d(TAG, "clicked on button to load blogs");
+        viewModel.getBlogsList();
     }
 }
